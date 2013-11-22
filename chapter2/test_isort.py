f2451269a00bf55964c2d1bcd4f6528fa0d6fb0a
Mitchell Peabody <mitchell.peabody@gmail.com>
2013-11-21 23:47:38 -0500

    Converted tests to class to avoid icky globals

--- chapter2/test_isort.py
+++ chapter2/test_isort.py
@@ -1,14 +1,18 @@
 import nose
 import random
+import unittest
 from isort import isort
 
+class ISortTests(unittest.TestCase):
+    def setUp(self):
+        self.pre_sort = range(1, 10)
+        random.shuffle(self.pre_sort)
 
-pre_sort = None
-def setup_func():
-    global pre_sort
-    pre_sort = range(1, 10)
-    random.shuffle(pre_sort)
+    def test_with_empty_list(self):
+        assert isort([]) == []
 
-@nose.with_setup(setup_func)
-def test_isort():
-    assert isort(pre_sort) == range(1, 10)
+    def test_with_random_list(self):
+        assert isort(self.pre_sort) == range(1, 10)
+
+    def test_with_reversed_list(self):
+        assert isort(range(9, 0, -1)) == range(1, 10)
