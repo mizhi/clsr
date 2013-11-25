6c43de53c7d77d1d61aabfcfdf3b4902535b08a3
Mitchell Peabody <mitchell.peabody@gmail.com>
2013-11-25 10:07:15 -0500

    Implemented inplace merging.

--- chapter2/merge_sorts.py
+++ chapter2/merge_sorts.py
@@ -1,20 +1,33 @@
 # Insertion Sort, PG 29
 #
-# Not a 1-1 translation of the algorithm. In fact, there are 3 separate
-# implementations. _merge_without_sentinel and _merge_with_sentinel
-# both create a new list to hold the merged lists.
+# There are 3 separate implementations of merge sort, each playing with
+# different ways of implementing the merge step.
 #
-# _merge_inplace is closer to the CLSR version. The version in CLSR mutates the
-# input list. That is, given a list A and indices indicating the left and right
-# boundaries of the sublists, it would copy them into temporary lists, L and R,
-# then merge those back into A.
+# _merge_without_sentinel and _merge_with_sentinel both create a new list to
+# hold the merged lists. _merge_without_sentinel is how I would most likely
+# implement merge sort the first go around. It seems the most obvious way of
+# implementing it in Python, though maybe not the most efficient.
 #
-# CLSR also employs the use of a sentinel value. This avoids the final two if
-# statements in the merge. This works fine with integers, but would be a bit
-# more difficult with arbitrary types. We can use None as our sentinel, but None
-# < any integer in Python. To work around this, the code fills in the merged
-# array from biggest to smallest rather than smallest to biggest. That is, it
-# merges from right to left instead of left to right.
+# _merge_with_sentinel was motivated by the way CLSR uses sentinels to detect
+# that a list has no more elements to merge. The implementation in CLSR assumes
+# integers, and in order to accomodate other datatypes, you would have to have
+# some value that is greater than everything. A simpler solution is to use None,
+# which is less than everything in Python2 (Python3 "fixed" this feature, so
+# I will need to figure out a new strategy).  In any case, by adopting this
+# convention, we need to merge from largest to smallest and the indices are
+# initialized and updated accordingly.
+#
+# _merge_inplace is closest to the CLSR implementation. The version in CLSR
+# mutates the input list. That is, given a list A and indices indicating the
+# left and right boundaries of the sublists, it would copy them into temporary
+# lists, L and R, then merge those back into A. _merge_inplace follows this
+# algorithm with the following differences.
+#
+# 1) CLSR arrays are 1-based indexed and the last index is typically the last
+#    element of the list. Python is 0-based and the last index is one past the
+#    last element of the list. We compensate accordingly.
+# 2) We allow l and r to go unspecified to ease the initial call to
+#    merge_sort_inplace
 
 def _merge_without_sentinel(list_1, list_2):
     i = 0
@@ -74,17 +87,32 @@ def merge_sort_with_sentinel(unsorted_list):
     return _merge_sort(unsorted_list, _merge_with_sentinel)
 
 
-
-
-
+def _merge_inplace(unsorted_list, l, m, r):
+    l_copy = [None] + unsorted_list[l:m]
+    r_copy = [None] + unsorted_list[m:r]
+    i = len(l_copy) - 1
+    j = len(r_copy) - 1
+    k = r - 1
+    while (l_copy[i] is not None) or (r_copy[j] is not None):
+        if l_copy[i] < r_copy[j]:
+            unsorted_list[k] = r_copy[j]
+            j -= 1
+        else:
+            unsorted_list[k] = l_copy[i]
+            i -= 1
+        k -= 1
 
 def merge_sort_inplace(unsorted_list, l = None, r = None):
-    l = l or 0
-    r = r or len(unsorted_list) - 1
+    if l is None:
+        l = 0
 
-    if l < r:
-        m = (l + r) // 2
+    if r is None:
+        r = len(unsorted_list)
+
+    m = (l + r) // 2
+
+    if l < r - 1:
         merge_sort_inplace(unsorted_list, l, m)
-        merge_sort_inplace(unsorted_list, m + 1, r)
+        merge_sort_inplace(unsorted_list, m, r)
         _merge_inplace(unsorted_list, l, m, r)
     return unsorted_list
--- chapter2/test_sorts.py
+++ chapter2/test_sorts.py
@@ -1,12 +1,18 @@
 import random
 from isort import isort
-from merge_sorts import merge_sort_without_sentinel, merge_sort_with_sentinel
+from merge_sorts import (merge_sort_without_sentinel,
+                         merge_sort_with_sentinel,
+                         merge_sort_inplace)
 
-SORT_FUNCS = (merge_sort_without_sentinel, merge_sort_with_sentinel, isort)
+
+SORT_FUNCS = (merge_sort_without_sentinel, merge_sort_with_sentinel,
+              merge_sort_inplace,
+              isort)
 
 class TestMergeSort(object):
     def setUp(self):
-        self.pre_sort = range(1, 10)
+        self.pre_sort = list(range(1, 10))
+        self.reversed_list = list(reversed(self.pre_sort))
         random.shuffle(self.pre_sort)
 
     def test_sorts(self):
@@ -22,4 +28,8 @@ class TestMergeSort(object):
         assert sort_func(self.pre_sort) == range(1, 10)
 
     def check_with_reversed_list(self, sort_func):
-        assert sort_func(range(9, 0, -1)) == range(1, 10)
+        assert sort_func(self.reversed_list) == range(1, 10)
+
+    def test_inplace_sort(self):
+        sorted_list = merge_sort_inplace(self.pre_sort)
+        assert id(sorted_list) == id(self.pre_sort)
